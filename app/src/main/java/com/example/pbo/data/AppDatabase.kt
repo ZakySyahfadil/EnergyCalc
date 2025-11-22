af1cc4c7a5e8759ecea90c1bf888d2bad173a66a
ZakySyahfadil <zakysyahfadil@gmail.com>
2025-11-22 15:29:13 +0800

    Menambahkan Database

---
+++ app/src/main/java/com/example/pbo/data/AppDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.pbo.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Account::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun accountDao(): AccountDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "user_database"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+}
