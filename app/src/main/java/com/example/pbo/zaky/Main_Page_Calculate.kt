a91f10da99af2db2dbebe18f885a3c42ff4b35b9
VeritasBA <muhammadnzwr15@gmail.com>
2025-11-22 03:22:42 -0800

    tambah UI dari main page

---
+++ app/src/main/java/com/example/pbo/zaky/Main_Page_Calculate.kt
@@ -0,0 +1,21 @@
+package com.example.pbo.zaky
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pbo.R
+
+class Main_Page_Calculate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_page_calculate)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
