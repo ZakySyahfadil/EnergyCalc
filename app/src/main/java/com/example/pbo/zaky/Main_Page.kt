b7f34c811e0883b4fb7e83e34971c6214624dfc5
ZakySyahfadil <134514827+ZakySyahfadil@users.noreply.github.com>
2025-11-30 08:33:16 +0800

    Revert "page_calculate"

--- app/src/main/java/com/example/pbo/zaky/Main_Page.kt
+++ app/src/main/java/com/example/pbo/zaky/Main_Page.kt
@@ -1,29 +1,19 @@
-package com.example.pbo.Nazwar
+package com.example.pbo.zaky
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.TestLooperManager
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.pbo.Alim.settingActivity
 import com.example.pbo.R
-import com.google.android.material.button.MaterialButton
 
-class MainPage : AppCompatActivity() {
+class Main_Page : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_page)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val tvName = findViewById<TextView>(R.id.name)
         val editProfile = findViewById<TextView>(R.id.edit)
@@ -36,14 +26,6 @@ class MainPage : AppCompatActivity() {
 
         editProfile.setOnClickListener {
             startActivity(Intent(this, settingActivity::class.java))
-        }
-
-        //Logika Perpindahan
-        val buttonCalculate = findViewById<MaterialButton>(R.id.button_calculate)
-
-        buttonCalculate.setOnClickListener {
-            val intent = Intent(this, Main_Page_Calculate::class.java)
-            startActivity(intent)
         }
     }
 }
