fe0012d9dec52b6471c3ccda0f0759d5ade5a526
ZakySyahfadil <zakysyahfadil@gmail.com>
2025-12-04 09:27:41 +0800

    XML History Result dan XML History Details

--- app/src/main/java/com/example/pbo/zaky/HistoryResults.kt
+++ app/src/main/java/com/example/pbo/zaky/HistoryResults.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import android.widget.ImageView
 import com.example.pbo.R
 
 class HistoryResults : AppCompatActivity() {
@@ -12,10 +14,6 @@ class HistoryResults : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_history_results)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
